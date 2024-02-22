46927a41f795938d02b5e5cd9cc2a897d575c932
teal <86630392+Tektonikal@users.noreply.github.com>
2024-02-22 11:42:41 -0500

    template

---
+++ obj/Debug/net472/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace MyFirstPlugin
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "MyFirstPlugin";
+        public const string PLUGIN_NAME = "MyFirstPlugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
