4a6179a9280938c85c6869de62ae95b14dc97b2a
june <145085206+succumbs@users.noreply.github.com>
2024-02-22 11:55:58 -0500

    init

--- obj/Debug/net472/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MyFirstPlugin
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "MyFirstPlugin";
-        public const string PLUGIN_NAME = "MyFirstPlugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
